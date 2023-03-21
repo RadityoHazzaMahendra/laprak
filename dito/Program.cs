c758de3452790bd4d7a3f3365f0c056be66b8e68
dito <radito.mahendra@students.amikom.ac.id>
2023-03-21 14:47:14 +0700

    laprak

---
+++ dito/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            //set nilai properties
+            taxi.DriverName = "dito ";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 10;
+            //pemanggilan method 
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPasanger();
+            Console.ReadKey();
+        }
+    }
